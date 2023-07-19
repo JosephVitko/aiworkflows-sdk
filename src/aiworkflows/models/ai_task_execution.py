a272dc99f2a263e96a297abba5a5265554d1af24
Joseph Vitko <josephvitko21@gmail.com>
2023-07-19 01:35:45 -0700

    add id to usage and execution

--- src/aiworkflows/models/ai_task_execution.py
+++ src/aiworkflows/models/ai_task_execution.py
@@ -12,6 +12,7 @@ class AiTaskExecution:
                  result: AiTaskDataObject = None,
                  success: bool = None,
                  completion_timestamp: int = None,
+                 execution_id: str = None,
                  ):
         """
         Initializes a new instance of the AiTaskExecution class.
@@ -20,11 +21,13 @@ class AiTaskExecution:
         :param result: The result of the task execution.
         :param success: Whether the task execution was successful.
         :param completion_timestamp: The timestamp of the task execution.
+        :param id: The id of the task execution.
         """
         self.callstack: list[AiTaskCall] = callstack
         self.result: AiTaskDataObject = result
         self.success: bool = success
         self.completion_timestamp: int = completion_timestamp
+        self.id: str = execution_id
 
     @staticmethod
     def from_json(json: dict) -> 'AiTaskExecution':
@@ -39,11 +42,13 @@ class AiTaskExecution:
         result = parse_optional_field(json, 'result', AiTaskDataObject)
         success = parse_optional_field(json, 'success', bool)
         completion_timestamp = parse_optional_field(json, 'completionTimestamp', int)
+        execution_id = parse_optional_field(json, 'id', str)
 
         return AiTaskExecution(callstack=callstack,
                                result=result,
                                success=success,
-                               completion_timestamp=completion_timestamp)
+                               completion_timestamp=completion_timestamp,
+                               execution_id=execution_id)
 
     def to_json(self) -> dict:
         """
@@ -56,6 +61,7 @@ class AiTaskExecution:
             'result': self.result,
             'success': self.success,
             'completionTimestamp': self.completion_timestamp,
+            'id': self.id
         }
 
     def __repr__(self):
